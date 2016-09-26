4433e6bc6bec0085773633c87bd9376d99024998
G33N <mceliz@enlaceit.com.ar>
2016-09-25 23:30:58 -0300

    Version final: borro conexion

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	$conexion = mysqli_connect("mysql.hostinger.com.ar","u238566687_admin","Seighei7","u238566687_inopt");
-  mysqli_query($conexion,"SET NAMES utf8");
-	 mysqli_query($conexion,"SET CHARACTER_SET utf");
-	$s='$';
-?>
